b1c731e3ee885cfa0e182cbe0a6762886281cbaa
Juan Garcia <jgarcia1545@gmail.com>
2020-01-15 00:55:20 -0800

    Worked on CSS

--- week01.php
+++ week01.php
@@ -1,4 +1,11 @@
 <?php
+
+// router.php
+if (preg_match('/\.(?:png|jpg|jpeg|gif)$/', $_SERVER["REQUEST_URI"])) {
+   return false;    // serve the requested resource as-is.
+} else { 
+   echo "<p>Welcome to PHP</p>";
+}
   // scriptname: week01.php
   $formOK = true;
   $firstname = $_POST['firstname'];
