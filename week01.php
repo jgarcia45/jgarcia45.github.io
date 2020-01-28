140e9602c6f29770ee22fd262732335d833cb304
Juan Garcia <jgarcia1545@gmail.com>
2020-01-27 18:18:29 -0800

    Fixed up index

--- week01.php
+++ week01.php
@@ -1,11 +1,4 @@
 <?php
-
-// router.php
-if (preg_match('/\.(?:png|jpg|jpeg|gif)$/', $_SERVER["REQUEST_URI"])) {
-   return false;    // serve the requested resource as-is.
-} else { 
-   echo "<p>Welcome to PHP</p>";
-}
   // scriptname: week01.php
   $formOK = true;
   $firstname = $_POST['firstname'];
